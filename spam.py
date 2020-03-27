bb09c26294059611bbcb0d34e142e5717313b19d
fweak1337 <freakgamer2331@gmail.com>
2020-03-27 13:43:33 -0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -5,139 +5,106 @@ init(convert=True)
 clear = lambda : os.system("cls")
 
 class Client:
-    def __init__(self, file):
-        super().__init__()
-        self.file = None
+    def __init__(self):
+        self.tokens = open(easygui.fileopenbox(msg="Please select the txt file for your tokens")).read().split("\n")
+        self.session = requests.Session()
 
     def RandomColor(self): 
         randcolor = random.randint(0x000000, 0xFFFFFF)
         return randcolor
-    
-    def spam(self, message: str, channelID: str, tts: bool, tokens: str, amount: int, delay: int):
 
-        jsonData = {
-            "content": " ",
-            "tts": tts,
-            "embed": {
-                    'title': message
-                }
-    
-        }
-        
+    def Headers (self, token: str):
         headers = {
             "Content-Type": "application/json",
-            "Content-Length": str(len(message)),
+            "authorization": token,
             "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 Mobile Safari/537.36"
         }
-        with requests.Session() as session:
+        return headers
+    
+    def spam(self, message: str, channelID: str, tts: bool, amount: int, delay: int):
 
-                for _i in range(amount):
-                    
-                    token = random.choice(tokens);
+        _message = {
+            "content": message,
+            "tts": tts,
+        }
+        
+        for token in self.tokens:
 
-                    session.put(f"https://discordapp.com/api/v6/users/@me/connections/skype/{random.randint(1, 10)}", json={ "name": 'icewallowcum,"visibility": 1, "verified": True },headers={"Authorization": token})
+            for _i in range(amount):
+                
+                time.sleep(delay)
 
-                    headers["authorization"] = token
+                _data = self.session.post(f"https://discordapp.com/api/v6/channels/{channelID}/messages", json=_message, headers=self.Headers(token))
 
-                    jsonData["embed"]["color"] = self.RandomColor()
-
-                    time.sleep(delay)
-
-                    req = session.post(f"https://discordapp.com/api/v6/channels/{channelID}/messages", json=jsonData, headers=headers)
-
-                    _data = req.json()
-
-                    if _data["id"]:
-                        print(f"[{Fore.BLUE}!!{Fore.RESET}] Sent the message.")
-
+                if _data.status_code == 200:
+                    print(f"[{Fore.CYAN}!!{Fore.RESET}] Sent a new message!")
+                else:
+                    ''
 
         self.start()
 
-    def join(self, code: str, tokens: str):
+    def join(self, code: str):
 
-        headers = {
-            "Content-Type": 'application/json',
-            "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.0;) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 Mobile Safari/537.36"
-        }
+        for token in self.tokens:
 
-        with requests.Session() as session:
+            _data = self.session.post(f"https://discordapp.com/api/v6/invites/{code}", headers=self.Headers(token)).json()
 
-            for token in tokens:
+            if _data.status_code == 200:
+                print(f"[{Fore.BLUE}!!{Fore.RESET}] Joined the {_data['guild']['name']} server.")
+            else:
+                print(f"[{Fore.RED}!!{Fore.RESET}] Could not join the guild on token: {token}")
 
-                headers["authorization"] = token
-
-                req = session.post(f"https://discordapp.com/api/v6/invites/{code}", headers=headers)
-
-                _data = req.json()
-        
-                if _data["guild"]:
-                    print(f"[{Fore.BLUE}!!{Fore.RESET}] Joined the {_data['guild']['name']} server.")
-
-
-            self.start()
+        self.start()
 
     def start(self):
-        if self.file == None:
+
+        if self.tokens == []:
             clear()
             print(f"[{Fore.BLUE}>{Fore.RESET}] Tokens File Selection {Fore.RESET}")
             time.sleep(1)
-            file = easygui.fileopenbox()
-            self.file = file
+            self.tokens = open(easygui.fileopenbox(msg="Please select the txt file for your tokens")).read().split("\n")
 
         clear()
+        print(f"{Fore.BLUE}[{Fore.RESET}1{Fore.BLUE}] Join Server {Fore.RESET}")
+        print(f"{Fore.BLUE}[{Fore.RESET}2{Fore.BLUE}] Spam channel {Fore.RESET}")
+        print(f"{Fore.BLUE}[{Fore.RESET}3{Fore.BLUE}] Exit Application {Fore.RESET}")
+        option = int(input("> "))
 
-        with open(self.file) as file:
-            tokens = file.read().split("\n")
+        if option not in [1,2,3]:
+            print(f"[{Fore.RED}Invalid Option{Fore.RESET}]")
+            time.sleep(1)
+            self.start()
 
-            print(f"{Fore.BLUE}[{Fore.RESET}0{Fore.BLUE}] Return to menu {Fore.RESET}")
-            print(f"{Fore.BLUE}[{Fore.RESET}1{Fore.BLUE}] Join Server {Fore.RESET}")
-            print(f"{Fore.BLUE}[{Fore.RESET}2{Fore.BLUE}] Spam channel {Fore.RESET}")
-            print(f"{Fore.BLUE}[{Fore.RESET}3{Fore.BLUE}] Exit Application {Fore.RESET}")
+        if option == 1:
+            clear()
+            print(f"[{Fore.BLUE}>{Fore.RESET}] Server Invite {Fore.RESET}")
+            code = str(input("> "))
 
-            option = int(input("> "))
+            if "https://discord.gg/" in code:
+                code = code.split("https://discord.gg/")[1]
+            else:
+                code = code 
+            
+            self.join(code)
 
-            if option not in [0,1,2,3]:
-                print(f"[{Fore.RED}Invalid Option{Fore.RESET}]")
-                time.sleep(1)
-                self.start()
+        if option == 2:
+            clear()
 
-            if option == 0:
-                clear()
-                self.start()
-                return
+            print(f"[{Fore.BLUE}>{Fore.RESET}] Channel Id {Fore.RESET}")
+            channelID = str(input("> "))
+            print(f"[{Fore.BLUE}>{Fore.RESET}] Message Content {Fore.RESET}")
+            message = str(input("> "))
+            print(f"[{Fore.BLUE}>{Fore.RESET}] Amount of messages to send {Fore.RESET}")
+            amount = int(input(" "))
+            print(f"[{Fore.BLUE}>{Fore.RESET}] Delay (1,2,3)[seconds] {Fore.RESET}")
+            delay = int(input(" "))
 
-            if option == 1:
-                clear()
+            self.spam(message=message, channelID=channelID, tts=False, amount=amount, delay=delay)
 
-                print(f"[{Fore.BLUE}>{Fore.RESET}] Server Invite {Fore.RESET}")
-                code = str(input("> "))
-
-                if "https://discord.gg/" in code:
-                    code = code.split("https://discord.gg/")[1]
-                else:
-                    code = code 
-                
-                self.join(code, tokens)
-
-            if option == 2:
-                clear()
-
-                print(f"[{Fore.BLUE}>{Fore.RESET}] Channel Id {Fore.RESET}")
-                channelID = str(input("> "))
-                print(f"[{Fore.BLUE}>{Fore.RESET}] Message Content {Fore.RESET}")
-                message = str(input("> "))
-                print(f"[{Fore.BLUE}>{Fore.RESET}] Amount of messages to send {Fore.RESET}")
-                amount = int(input(" "))
-                print(f"[{Fore.BLUE}>{Fore.RESET}] Delay (1,2,3)[seconds] {Fore.RESET}")
-                delay = int(input(" "))
-
-
-                self.spam(message, channelID, False, tokens, amount, delay)
-
-            if option == 3:
-                clear()
-                exit()
+        if option == 3:
+            clear()
+            exit()
             
 
 if __name__ == "__main__":
-    Client(None).start()
+    Client().start()
